56c7c3c6060c82405ceb27f3a086a2df8c7dab27
'Parth' <'parthbansal2374@gmail.com'>
2026-08-14 10:33:34 +0530

    Add button UI to new.jsx

--- new.jsx
+++ new.jsx
@@ -1,1 +1,12 @@
-console.log("Hello from new.jsx!");
+import React from 'react';
+
+const NewPage = () => {
+  return (
+    <div>
+      <h1>New Page</h1>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </div>
+  );
+};
+
+export default NewPage;
