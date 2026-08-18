f14eccdeda9c01e6de44b6e090390bf47b69c290
'Parth' <'parthbansal2374@gmail.com'>
2026-08-18 16:44:49 +0530

    Add delete.jsx file for landing page

---
+++ delete.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Our Landing Page</h1>
+      <p>This is the main content of the landing page.</p>
+      {/* Add more landing page elements here */}
+    </div>
+  );
+};
+
+export default LandingPage;
