2dd8d248ddd66e33895409675af7cfa7c8619ad2
'Parth' <'parthbansal2374@gmail.com'>
2026-08-24 10:50:20 +0530

    feat: Add delete.jsx with landing page component

---
+++ delete.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page!</h1>
+      <p>This is a simple landing page created in delete.jsx.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
